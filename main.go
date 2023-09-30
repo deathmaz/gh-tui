8b9e718b4f3a11c1e786e48a99711a5c03059665
deathmaz <00maz1987@gmail.com>
2023-09-30 21:06:26 +0300

    feat: show pr details

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strings"
+	"time"
 
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
@@ -20,6 +22,11 @@ type Owner struct {
 	Login string
 }
 
+const (
+	prs         = "prs"
+	description = "description"
+)
+
 type repo struct {
 	NameWithOwner string `json:"nameWithOwner"`
 	Name          string `json:"name"`
@@ -29,13 +36,53 @@ type repo struct {
 var docStyle = lipgloss.NewStyle().Margin(1, 2)
 
 type PullRequest struct {
-	Url    string
-	Title  string
-	Number int
-	State  string
-	Author struct {
+	Url         string
+	CreatedAt   string
+	Title       string
+	Number      int
+	BaseRefName string
+	Author      struct {
 		Login string
 	}
+}
+
+type ReviewRequest struct {
+	RequestedReviewer struct {
+		User struct {
+			Login string
+		} `graphql:"... on User"`
+	}
+}
+type PullRequestDetails struct {
+	Url         string
+	Body        string
+	State       string
+	CreatedAt   string
+	Title       string
+	Number      int
+	HeadRefName string
+	BaseRefName string
+	Author      struct {
+		Login string
+	}
+	ReviewRequests struct {
+		Nodes []ReviewRequest
+	} `graphql:"reviewRequests(first:10)"`
+}
+
+type PullRequestDetailsRequest struct {
+	Url         string
+	Body        string
+	State       string
+	CreatedAt   string
+	Title       string
+	Number      int
+	HeadRefName string
+	BaseRefName string
+	Author      struct {
+		Login string
+	}
+	ReviewRequests []ReviewRequest `graphql:"reviewRequests(first:10)"`
 }
 
 type item struct {
@@ -48,15 +95,23 @@ func (i item) Description() string { return i.desc }
 func (i item) FilterValue() string { return i.title }
 
 type app struct {
-	list   list.Model
-	prList []PullRequest
-	err    error
-	cursor int
-	repo   repo
+	list         list.Model
+	prList       []PullRequest
+	err          error
+	repo         repo
+	currentView  string
+	prDetails    PullRequestDetails
+	changedFiles string
+}
+
+type PrDescription struct {
+	details      PullRequestDetails
+	changedFiles string
 }
 
 type (
 	listMsg []PullRequest
+	prMsg   PrDescription
 	errMsg  struct{ err error }
 )
 
@@ -75,7 +130,7 @@ func (m app) getViaRestClient() tea.Msg {
 	return listMsg(response)
 }
 
-func (m app) getViaGraphQL() tea.Msg {
+func (m app) getPulls() tea.Msg {
 	client, err := api.DefaultGraphQLClient()
 	if err != nil {
 		return errMsg{err}
@@ -99,13 +154,83 @@ func (m app) getViaGraphQL() tea.Msg {
 	return listMsg(query.Repository.PullRequests.Nodes)
 }
 
+func (m app) getPull(number int) tea.Msg {
+	client, err := api.DefaultGraphQLClient()
+	if err != nil {
+		return errMsg{err}
+	}
+	var query struct {
+		Repository struct {
+			PullRequest PullRequestDetails `graphql:"pullRequest(number: $number)"`
+		} `graphql:"repository(owner: $owner, name: $name)"`
+	}
+	variables := map[string]interface{}{
+		"number": graphql.Int(number),
+		"owner":  graphql.String(m.repo.Owner.Login),
+		"name":   graphql.String(m.repo.Name),
+	}
+	err = client.Query("PullRequest", &query, variables)
+	if err != nil {
+		return errMsg{err}
+	}
+	args := []string{
+		"pr",
+		"diff",
+		fmt.Sprintf("%d", number),
+		"-R",
+		m.repo.NameWithOwner,
+		"--name-only",
+	}
+	stdOut, _, err := gh.Exec(args...)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return prMsg(PrDescription{
+		details:      query.Repository.PullRequest,
+		changedFiles: stdOut.String(),
+	})
+}
+
 func (m app) View() string {
-	return docStyle.Render(m.list.View())
+	if m.currentView == prs {
+		return docStyle.Render(m.list.View())
+	} else if m.currentView == description {
+		s := strings.Builder{}
+		s.WriteString(m.prDetails.Title)
+		s.WriteString("\n")
+		s.WriteString(fmt.Sprintf(
+			"%s wants to merge xx commits into %s from %s",
+			m.prDetails.Author.Login,
+			m.prDetails.BaseRefName,
+			m.prDetails.HeadRefName,
+		))
+		s.WriteString("\n")
+
+		body := "No description provided"
+		if m.prDetails.Body != "" {
+			body = m.prDetails.Body
+		}
+		s.WriteString(body)
+		s.WriteString("\n")
+
+		for _, r := range m.prDetails.ReviewRequests.Nodes {
+			s.WriteString(fmt.Sprintf("%s", r.RequestedReviewer.User.Login))
+			s.WriteString("\n")
+		}
+
+		s.WriteString(m.changedFiles)
+		s.WriteString("\n")
+
+		return docStyle.Render(s.String())
+	}
+
+	return "Loading..."
 }
 
 func (m app) Init() tea.Cmd {
 	// return m.getViaRestClient
-	return m.getViaGraphQL
+	return m.getPulls
 }
 
 func (m app) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -114,7 +239,6 @@ func (m app) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		switch msg.String() {
 		case "d":
 			selItem := m.list.SelectedItem().(item)
-			fmt.Printf("%d", selItem.Number)
 			c := exec.Command(
 				"gh",
 				"pr",
@@ -127,37 +251,47 @@ func (m app) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				return errMsg{err: err}
 			})
 			return m, cmd
-		case "v":
-			selItem := m.list.SelectedItem().(item)
-			fmt.Printf("%d", selItem.Number)
-			c := exec.Command(
-				"gh",
-				"pr",
-				"view",
-				fmt.Sprintf("%d", selItem.Number),
-				"-R",
-				m.repo.NameWithOwner,
-			)
-			cmd := tea.ExecProcess(c, func(err error) tea.Msg {
-				return errMsg{err: err}
-			})
-			return m, cmd
+		case "i":
+			return m, func() tea.Msg {
+				item := m.list.SelectedItem().(item)
+				return m.getPull(item.Number)
+			}
+		case "h":
+			if m.currentView == description {
+				m.currentView = prs
+				return m, nil
+			}
 		}
 	case tea.WindowSizeMsg:
 		h, v := docStyle.GetFrameSize()
 		m.list.SetSize(msg.Width-h, msg.Height-v)
+	case prMsg:
+		m.prDetails = PullRequestDetails(msg.details)
+		m.changedFiles = msg.changedFiles
+		m.currentView = description
 	case listMsg:
 		items := make([]list.Item, 0, len(msg))
 		for _, p := range msg {
+			date, _ := time.Parse(time.RFC3339, p.CreatedAt)
 			items = append(items, item{
-				title:  p.Title,
-				desc:   fmt.Sprintf("%s %d", p.Author.Login, p.Number),
+				title: fmt.Sprintf("%s", p.Title),
+				desc: fmt.Sprintf(
+					"#%d opened on %d-%02d-%02d %02d:%02d by %s",
+					p.Number,
+					date.Day(),
+					date.Month(),
+					date.Year(),
+					date.Hour(),
+					date.Minute(),
+					p.Author.Login,
+				),
 				Number: p.Number,
 			})
 		}
 		m.list.SetItems(items)
 		m.prList = msg
 	case errMsg:
+		fmt.Printf("%s", msg)
 		m.err = msg
 		return m, nil
 	}
@@ -173,8 +307,9 @@ func main() {
 	}
 	var items []list.Item
 	a := app{
-		list: list.New(items, list.NewDefaultDelegate(), 0, 0),
-		repo: repo,
+		list:        list.New(items, list.NewDefaultDelegate(), 0, 0),
+		repo:        repo,
+		currentView: prs,
 	}
 	p := tea.NewProgram(a, tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
